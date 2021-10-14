bf0f1dcc03e552b3473560c15ee3f3e854d67fa3
Isidro Arias <isidroariass@hotmail.es>
2021-10-14 16:31:35 +0200

    refactor toc

--- src/code_to_pdf/toc_generator.py
+++ src/code_to_pdf/toc_generator.py
@@ -3,6 +3,7 @@ import os
 import re
 import tempfile
 from collections import namedtuple
+from typing import List
 
 import pdfkit
 from art import text2art
@@ -12,28 +13,46 @@ from jinja2 import Template
 
 from code_to_pdf.pdf_generator import PDFKIT_OPTIONS, PDFCreator
 
-ENTRY_DIR = """<div class=row>{{ 157*'.' }}<span class="row_text"> {{tree}}<span class="dir">{{ name }} </span>&nbsp</span><span class="right">{{ page }}</div>"""
-ENTRY_FILE = """<div class=row>{{ 157*'.' }}<span class="row_text">{{tree}} <span class="file">{{ name }}&nbsp</span></span><span class="right">{{ page }}</div>"""
-
 GitInfo = namedtuple("GitInfo", "commit datetime branch")
 
 
+class Entry:
+    ENTRY_DIR = """<div class=row>{{ 157*'.' }}<span class="row_text"> {{tree}}<span class="dir">{{ name }} </span>&nbsp</span><span class="right">{{ page }}</div>"""
+    ENTRY_FILE = """<div class=row>{{ 157*'.' }}<span class="row_text">{{tree}} <span class="file">{{ name }}&nbsp</span></span><span class="right">{{ page }}</div>"""
+
+    def __init__(self, name="", depth=0, page=0, tree="", is_dir=False):
+        self.name = name
+        self.depth = depth
+        self.page = depth
+
+        tree = re.sub(" ", "&nbsp;&nbsp;", tree)
+        self.tree = tree
+
+        self.is_dir = is_dir
+
+    def render_html(self) -> str:
+        if self.is_dir:
+            template = Template(self.ENTRY_DIR)
+        else:
+            template = Template(self.ENTRY_FILE)
+        return template.render(
+            name=self.name, depth=self.depth, page=self.page, tree=self.tree
+        )
+
+
 class TocGenerator:
-    def __init__(self):
+    def __init__(self, max_pages_per_volume=300):
         self.entries = ""
+        self.max_pages_per_volume = max_pages_per_volume
+        self.entries_list: List[Entry] = []
 
     def add_entry(self, name, depth, page, tree, is_dir=False):
-        tree = re.sub(" ", "&nbsp;&nbsp;", tree)
         if is_dir:
-            template = Template(ENTRY_DIR)
             logging.info(depth * "   " + "Folder: {}".format(name))
         else:
             logging.info((depth + 1) * "   " + "File: {}: {}".format(name, page))
-            template = Template(ENTRY_FILE)
-        self.entries = (
-            self.entries
-            + template.render(name=name, depth=depth, page=page, tree=tree)
-            + "\n"
+        self.entries_list.append(
+            Entry(name=name, depth=depth, page=page, tree=tree, is_dir=is_dir)
         )
 
     @staticmethod
@@ -66,8 +85,12 @@ class TocGenerator:
             else None
         )
 
+        entries = ""
+        for it in self.entries_list:
+            entries += it.render_html() + "\n"
+
         output_html = template.render(
-            entries=self.entries,
+            entries=entries,
             page_number_pos=800,
             ascii_title=ascii_title,
             git_info=git_info,
@@ -79,3 +102,6 @@ class TocGenerator:
             PDFCreator.add_blank_page(output_pdf)
 
         return output_pdf
+
+    def generate_volumes(output_path: str):
+        pass
