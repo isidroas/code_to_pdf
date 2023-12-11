9b4fe6fe5426c0133d7332c4645425b74728f5c7
Isidro Arias <isidroariass@hotmail.es>
2023-12-11 14:22:38 +0100

    parametrized code

--- latex/generate.py
+++ latex/generate.py
@@ -4,10 +4,23 @@ from jinja2.loaders import FileSystemLoader
 from latex import build_pdf
 from latex.jinja2 import make_env
 
+codes = [  # section; label; absolute_path
+    (
+        r"code\_to\_pdf/run.py",
+        "sec:code-to-pdf:run.py",
+        "/home/isidro/mp/code_to_pdf/src/code_to_pdf/run.py",
+    ),
+    (
+        r"code\_to\_pdf/template.html",
+        "sec:code-to-pdf:template.html",
+        "/home/isidro/mp/code_to_pdf/src/code_to_pdf/template.html",
+    ),
+]
+
 env = make_env(loader=FileSystemLoader("."))
 tpl = env.get_template("doc.tex")
 
-generated = tpl.render(name="Alice")
+generated = tpl.render(name="Alice", codes=codes)
 
 
 pdf = build_pdf(generated)
