8f43e231cd0a7a63b7ebf1b12eb762e7165e5eae
Isidro Arias <isidroariass@hotmail.es>
2023-12-17 11:18:45 +0100

    print relative

--- latex/generate.py
+++ latex/generate.py
@@ -1,4 +1,3 @@
-# TODO: convert template.tex to generated.tex
 from pathlib import Path
 
 from jinja2.loaders import FileSystemLoader
@@ -26,31 +25,29 @@ def get_codes_and_nodes(root):
         ],
         exclude_dirs=["venv", "build", ".git", "*.egg-info"],
     ):
-        print(path)
         relative = path.relative_to(root)
         if path.is_file():
-            code = str(path)
-            codes.append(code)
+            codes.append({"abs": str(path), "rel": str(relative)})
         node = (
             len(relative.parts) + 1,
             path.name,
             str(path) if path.is_file() else None,
-        )  # TODO: remove hard depth
-        # print(node)
+        )
         nodes.append(node)
     return codes, nodes
 
 
-codes, nodes = get_codes_and_nodes(Path("/home/isidro/mp/code_to_pdf/src"))
+if __name__ == "__main__":
+    codes, nodes = get_codes_and_nodes(Path("/home/isidro/mp/code_to_pdf/"))
 
+    env = make_env(loader=FileSystemLoader("."))
+    tpl = env.get_template("doc.tex")
 
-env = make_env(loader=FileSystemLoader("."))
-tpl = env.get_template("doc.tex")
+    generated = tpl.render(codes=codes, nodes=nodes)
 
-generated = tpl.render(codes=codes, nodes=nodes)
-with open("/tmp/out.tex", "wt") as file:
-    file.write(generated)
+    # for debugging
+    with open("/tmp/out.tex", "wt") as file:
+        file.write(generated)
 
-
-pdf = build_pdf(generated)
-pdf.save_to("generated.pdf")
+    pdf = build_pdf(generated)
+    pdf.save_to("generated.pdf")
