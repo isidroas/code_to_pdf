c2097e80a395b39295a540a1d8a161c7a0d0ab95
Isidro Arias <isidroariass@hotmail.es>
2023-12-17 10:44:17 +0100

    relative to

--- latex/generate.py
+++ latex/generate.py
@@ -7,52 +7,43 @@ from walkfind import walkfind
 from latex import build_pdf
 from latex.jinja2 import make_env
 
-codes = [  # section; label; absolute_path
-    (
-        "code_to_pdf/run.py",
-        "code-to-pdf:run.py",
-        "/home/isidro/mp/code_to_pdf/src/code_to_pdf/run.py",
-    ),
-    (
-        r"code_to_pdf/template.html",
-        "code-to-pdf:template.html",
-        "/home/isidro/mp/code_to_pdf/src/code_to_pdf/template.html",
-    ),
-]
 
-nodes = [
-    (1, "run.py", "sec:code-to-pdf:run.py"),
-    (2, "template.html", "sec:code-to-pdf:template.html"),
-]
+def get_codes_and_nodes(root):
+    codes = []
+    nodes = []
 
-codes = []
-nodes = []
+    for path in walkfind(
+        root,
+        # git_tracked=True,
+        also_dirs=True,
+        exclude_files=[
+            "tree_generator.py",
+            "*.pyc",
+            "*.pdf",
+            "tree.py",
+            ".coverage",
+            "output.html",
+        ],
+        exclude_dirs=["venv", "build", ".git", "*.egg-info"],
+    ):
+        print(path)
+        relative = path.relative_to(root)
+        if path.is_file():
+            code = str(path)
+            codes.append(code)
+        node = (
+            len(relative.parts) + 1,
+            path.name,
+            str(path) if path.is_file() else None,
+        )  # TODO: remove hard depth
+        # print(node)
+        nodes.append(node)
+    return codes, nodes
 
-for path in walkfind(
-    Path("/home/isidro/mp/code_to_pdf/src"),
-    # git_tracked=True,
-    also_dirs=True,
-    exclude_files=[
-        "tree_generator.py",
-        "*.pyc",
-        "*.pdf",
-        "tree.py",
-        ".coverage",
-        "output.html",
-    ],
-    exclude_dirs=["venv", "build", ".git", "*.egg-info"],
-):
-    print(path)
-    if path.is_file():
-        code = str(path)
-        codes.append(code)
-    node = (
-        len(path.parts) - 4,
-        path.name,
-        str(path) if path.is_file() else None,
-    )  # TODO: remove hard depth
-    # print(node)
-    nodes.append(node)
+
+codes, nodes = get_codes_and_nodes(Path("/home/isidro/mp/code_to_pdf/src"))
+
+
 env = make_env(loader=FileSystemLoader("."))
 tpl = env.get_template("doc.tex")
 
