b881b24f68be4de35af598cd2dd9522a3a53917d
Isidro Arias <isidroariass@hotmail.es>
2024-12-01 12:38:13 +0100

    convert bulk

---
+++ examples/convert_bulk_markdown_to_pdf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for file in $(find  $1  -name '*.md' | sort); do
+    pushd $(dirname $file)
+    pandoc $(basename $file) -o $(basename --suffix=.md $file).pdf
+    rm $(basename $file)
+    popd
+done
