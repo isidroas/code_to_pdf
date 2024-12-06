677ea23179cb6bbfacf9462e2c155c5d48cadc0c
Isidro Arias <isidroariass@hotmail.es>
2024-12-06 10:47:09 +0100

    nerd fonts

---
+++ examples/install_nerd_fonts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# # TODO: completion bash
+# # TODO: create debian package
+# if [ $UID -ne 0 -a $EUID -ne 0 ]; then
+#     echo run as sudo. $UID $EUID
+#     exit 1
+# fi
+
+
+# PREFIX=/usr/local/share/fonts
+PREFIX=~/.local/share/fonts
+for font in DejaVuSansMono  Hack  SourceCodePro FiraMono FiraCode; do
+
+    if [ -d $PREFIX/$font ]; then
+        continue
+    fi
+
+    mkdir -p $PREFIX/$font
+    curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/${font}.zip > /tmp/${font}.zip
+    # unzip -o /tmp/${font}.zip -d $PREFIX/$font
+done
