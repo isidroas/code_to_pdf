9a4d166f55694464c41e7fc8290af8ae47c0491e
Isidro Arias <isidroariass@hotmail.es>
2024-12-08 15:03:46 +0100

    moved install

--- .github/workflows/install_nerd_fonts.sh
+++ .github/workflows/install_nerd_fonts.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-
+PREFIX=~/.local/share/fonts
+# other possible locations:
 # PREFIX=/usr/local/share/fonts
 # PREFIX=/usr/share/fonts
-PREFIX=~/.local/share/fonts
-for font in DejaVuSansMono  Hack  SourceCodePro FiraMono FiraCode; do
 
+for font in DejaVuSansMono  Hack  SourceCodePro FiraMono FiraCode; do
     if [ -d $PREFIX/$font ]; then
         continue
     fi
-
     curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/${font}.zip > /tmp/${font}.zip
     mkdir -p $PREFIX/$font
     unzip -o /tmp/${font}.zip -d $PREFIX/$font
 done
 
-fc-match  'SauceCodePro Nerd Font'
+# fc-match  'SauceCodePro Nerd Font'
