a0f824889a287481f17f4f3a80df2c6be1ec082b
Isidro Arias <isidroariass@hotmail.es>
2024-12-08 12:30:36 +0100

    locally

--- examples/install_nerd_fonts.sh
+++ examples/install_nerd_fonts.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
 
-# # TODO: completion bash
-# # TODO: create debian package
-# if [ $UID -ne 0 -a $EUID -ne 0 ]; then
-#     echo run as sudo. $UID $EUID
-#     exit 1
-# fi
-
 
 # PREFIX=/usr/local/share/fonts
-PREFIX=/usr/share/fonts
-# PREFIX=~/.local/share/fonts
+# PREFIX=/usr/share/fonts
+PREFIX=~/.local/share/fonts
 for font in DejaVuSansMono  Hack  SourceCodePro FiraMono FiraCode; do
 
     if [ -d $PREFIX/$font ]; then
@@ -23,7 +16,4 @@ for font in DejaVuSansMono  Hack  SourceCodePro FiraMono FiraCode; do
     unzip -o /tmp/${font}.zip -d $PREFIX/$font
 done
 
-
-sudo apt install tree
-tree $PREFIX
 fc-match  'SauceCodePro Nerd Font'
