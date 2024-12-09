57b16b53c477c0c7758062435081bc62b7c4bcd0
Isidro Arias <isidroariass@hotmail.es>
2024-12-09 20:29:01 +0100

    explain pandoc

--- examples/convert_bulk_markdown_to_pdf.sh
+++ examples/convert_bulk_markdown_to_pdf.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
+
+# quita los margenes ya que el documento padre añadirá los suyos. En inferior no es 0 para no eleminar el page number
+TEMPLATE_OPTS='-V geometry:bottom=1.5cm,left=0cm,top=0cm,right=0cm'
+
+# this document will be probally printed. So cliclable links are useless
+TEMPLATE_OPTS+=' -V links-as-notes=true'
+
 for file in $(find  $1  -name '*.md' | sort); do
     pushd $(dirname $file)
-    pandoc $(basename $file) -V geometry:bottom=1.5cm,left=0cm,top=0cm,right=0cm -V links-as-notes=true --from=gfm -o $(basename --suffix=.md $file).pdf
+    pandoc $(basename $file) ${TEMPLATE_OPTS[@]}  --from=gfm -o $(basename --suffix=.md $file).pdf
     rm $(basename $file)
     popd
 done
