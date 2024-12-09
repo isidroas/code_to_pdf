5a1e78eee3cf1b8518edce82580a5babaf01481a
Isidro Arias <isidroariass@hotmail.es>
2024-12-09 17:52:55 +0100

    pandoc options no margin

--- examples/convert_bulk_markdown_to_pdf.sh
+++ examples/convert_bulk_markdown_to_pdf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 for file in $(find  $1  -name '*.md' | sort); do
     pushd $(dirname $file)
-    pandoc $(basename $file) -o $(basename --suffix=.md $file).pdf
+    pandoc $(basename $file) -V geometry:bottom=1.5cm,left=0cm,top=0cm,right=0cm -V links-as-notes=true --from=gfm -o $(basename --suffix=.md $file).pdf
     rm $(basename $file)
     popd
 done
